146fca09eea10aef3a2a159b2142e586cef71af7
maddockz <maddockz@gmail.com>
2015-03-31 18:19:56 -0700

    Added config file

---
+++ config.py
@@ -0,0 +1,11 @@
+__author__ = 'maddockz'
+
+# MySQL configuration
+database = {'host':'localhost',
+            'user':'usr_wikifun',
+            'passwd':'jimmywales',
+            'db':'wikifun'}
+
+# Name of wikipedia archival dump file
+# This file must be in ./data/ directory
+wiki_dump = "enwiki-20150304-pages-articles.xml.bz2"
