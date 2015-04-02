ff5dfe66f01c3f9b16b47f487517dbb13c92dcbd
maddockz <maddockz@gmail.com>
2015-04-01 19:03:53 -0700

    Cleaned up code to better adhere to PEP-8 style guidelines.  Also made bz2_parse function to add functionality to parse the bz2  and not directly pipe it to a mysql table.

--- wikifun/etl/parse_xml.py
+++ wikifun/etl/parse_xml.py
@@ -1,45 +1,88 @@
-'''
+"""
 Created on Mar 28, 2015
 Parses XML file, extracting article text and wikipedia categories
 @author: maddockz
-'''
+"""
 
-import pkg_resources
 import re
 from bz2 import BZ2File
 try:
-    import xml.etree.cElementTree as ET
+    import xml.etree.cElementTree as et
 except ImportError:
-    import xml.etree.ElementTree as ET
-import config
+    import xml.etree.ElementTree as et
 
-_wiki_dump = config.wiki_dump
-wiki_dump_filename = pkg_resources.resource_filename('wikifun',
+import pkg_resources
+import peewee as pw
+
+import config
+import wikifun.etl.data_models as dm
+
+_wiki_dump = config.WIKI_DUMP_FILENAME
+_wiki_dump_abspath = pkg_resources.resource_filename('wikifun',
                                                      "../data/" + _wiki_dump)
 
-def bz2_to_mysql(filename):
+def insert_record(record):
+    """
+    INSERT record into MySQL database
+    :param record: dict
+    :key title: unicode
+    :key text: unicode
+    :key cats: list[unicode]
+    :return: None
+    """
+    dm.db.connect()
+    try:
+        print record['title'], record['cats']  # For TESTING
+        article = dm.Article(title=record['title'], text=record['text'])
+        article.save()
+        for cat_value in record['cats']:
+            category = dm.Category(value=cat_value)
+            category.save()
+            mapping_table = dm.MappingTable(article=article,
+                                            category=category)
+            mapping_table.save()
+    except pw.IntegrityError, e:
+        print e
+    except pw.OperationalError, e:
+        print u'Error with article {0}'.format(record['title'])
+        # raise e
+    finally:
+        dm.db.close()
+
+def bz2_parse(filename=_wiki_dump_abspath, insert_fun=insert_record, limit=1e30):
     """
     Parses a .xml.bz2 file containing wikipedia articles and stores in MySQL
     :param filename: str # path to .xml.bz2 file
+    :param insert_fun: function( list[unicode] ) # to record the articles
+    :param limit: int # Maximum number of articles to output
     :return: None
     """
     with BZ2File(filename) as f:
+        # Read in first page
         page = read_page(f)
-        N = 0 ## REMOVE N LATER
-        while page and N < 100:
-            text = extract_text(page)
-            title = extract_title(page)
-            cats = categories_from_text(text)
-            try:
-                # INSERT only if article text is not empty
-                if text:
-                    print 'TEXT ACCEPTED FROM: {0}'.format(title)
-                    print 'TEXT IS TYPE: {0}'.format(type(text))
-                    insert_record(title,text,cats)
-            except:
-                "Error INSERTing article '{0}'".format(title)
+        # Create tables if do not exist
+        dm.create_tables()
+
+        n = 0
+        while page and n < limit:
+            record = {'text': extract_text(page), 'title': extract_title(page)}
+            record['cats'] = categories_from_text(record['text'])
+            # INSERT only if article text is not empty
+            if record['text']:
+                insert_fun(record)
             page = read_page(f)
-            N += 1
+            n += 1
+
+
+def bz2_to_mysql(filename = _wiki_dump_abspath, limit=1e30):
+    """
+    Parses a .xml.bz2 file containing wikipedia articles and stores in MySQL
+    :param filename: str # path to .xml.bz2 file
+    :param limit: int # Max number of articles to record
+    :return: None
+    """
+    bz2_parse(filename, insert_record, limit)
+
 
 def read_page(f):
     """
@@ -53,15 +96,15 @@ def read_page(f):
     # Discard lines until <page> or EOF is encountered
     while not page_open:
         line = f.readline()
-        if(re.findall('<page>',line)):
+        if re.findall('<page>', line):
             page_open = True
             lines.append('<page>')
         if not line:
-            break # EOF reached
+            break  # EOF reached
     # Store lines until after </page> encountered
     while page_open:
         line = f.readline()
-        if(re.findall('</page>', line)):
+        if re.findall('</page>', line):
             page_open = False
         if not line:
             print "Warning: EOF reached within <page> environment"
@@ -76,12 +119,14 @@ def extract_text(xml_string):
     :type xml_string: str # a single wikipedia XML page
     :return: unicode # the raw text of the wikipedia article
     """
-    root = ET.fromstring(xml_string)
+    root = et.fromstring(xml_string)
     raw_text = root.find('./revision/text').text
-    ## Remove meta data before article text
+    # Remove meta data before article text
     match = re.search("\'\'\'", raw_text)
     if match:
-        clean_text = unicode(raw_text[match.start():])
+        clean_text = raw_text[match.start():]
+        if type(clean_text) is not unicode:
+            clean_text = unicode(clean_text, 'utf-8')
     else:
         clean_text = u''
     return clean_text
@@ -98,14 +143,19 @@ def categories_from_text(text):
     # [[Category:<name>| ]] (in case <name> is article title)
     return re.findall('\[\[[Cc]ategory:\s*([^\]\|]*)\s*\|?\s*\]\]', text)
 
+
 def extract_title(xml_string):
     """
     Extracts the title from XML Wikipedia page string
     :param xml_string: unicode
     :return: unicode
     """
-    root = ET.fromstring(xml_string)
-    return unicode(root.find('./title').text)
+    root = et.fromstring(xml_string)
+    title = root.find('./title').text
+    if type(title) is not unicode:
+        title = unicode(title, 'utf-8')
+    return title
+
 
 def title_from_text(text):
     """
@@ -121,15 +171,4 @@ def title_from_text(text):
     except IndexError:
         return ''
 
-def insert_record(title, text, cats):
-    """
-    INSERT entry into MySQL database
-    :param title: str
-    :param text: str
-    :param cats: list[str]
-    :return: None
-    """
-    print u"Title:{0}, Cats:{1},\n Text:{2}...".format(title, cats,
-                                                      text[:80].replace('\n',''))
-    pass
 
