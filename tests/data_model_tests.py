c7179fc5c7c218d903a2128285bf3d8224462a14
maddockz <maddockz@gmail.com>
2015-04-01 19:06:25 -0700

    added tests that check create_tables function

--- tests/data_model_tests.py
+++ tests/data_model_tests.py
@@ -1,1 +1,16 @@
 __author__ = 'maddockz'
+
+import wikifun.etl.data_models as dm
+
+class TestDatabaseConnection:
+    def setUp(self):
+        dm.db.connect()
+
+    def teardown(self):
+        dm.db.close()
+
+    def test_create_table(self):
+        dm.create_tables()
+        tables = dm.db.get_tables()
+        for table in [u'article', u'category', u'mappingtable']:
+            assert table in tables, "missing TABLE {0}".format(table)
