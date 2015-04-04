af4a0dfb6c11cd86a8213830ebf8b1c3fc7d4235
maddockz <maddockz@gmail.com>
2015-04-03 19:13:31 -0700

    Added test for 'Apollo' article, which is an article that is currently failing to upload due, I'm guessing, to a problem with unicode and mysql

--- tests/data_model_tests.py
+++ tests/data_model_tests.py
@@ -14,3 +14,12 @@ class TestDatabaseConnection:
         tables = dm.db.get_tables()
         for table in [u'article', u'category', u'mappingtable']:
             assert table in tables, "missing TABLE {0}".format(table)
+
+    def test_for_Apollo_article(self):
+        try:
+            dm.Article.get(dm.Article.title==u'Apollo')
+        except dm.Article.DoesNotExist, e:
+            assert False, e
+        else:
+            assert True
+
