ed7d03310fac2ae36d20630bca7308357f2ed04c
maddockz <maddockz@gmail.com>
2015-04-09 17:19:41 -0700

    added basic tests for word_count module

---
+++ tests/word_count_tests.py
@@ -0,0 +1,38 @@
+__author__ = 'maddockz'
+
+import wikifun.etl.word_count as wc
+import wikifun.etl.data_models as dm
+
+class TestWordCounts:
+    def setUp(self):
+        self.text="""
+                The word A is written twice A?
+                Word appears twice?
+                Twice appears thrice.
+                Appears[appears] as well.
+                """
+        self.anarchy = dm.Article.get(dm.Article.title == 'Anarchism')
+
+    def test_basic_count(self):
+        freqs = wc.count(self.text)
+        assert freqs['a'] == 2
+        assert freqs['twice'] == 3
+        assert freqs['appears'] == 4
+        assert freqs['Twice'] == 0
+
+    def test_anarchy_count(self):
+        freqs = wc.count(self.anarchy.text)
+        assert freqs['absurdity'] == 1
+        assert None not in freqs.keys()
+
+    def test_tabulate_articles(self):
+        # Again test that 'Anarchy' article contains 'absurdity'
+        # exactly once, but this time stored in the database.
+        wc.tabulate_articles(0,1)  # Tabulate 'Anarchy' article
+        absurd = dm.Word.get(dm.Word.value == "absurdity")
+        wf = dm.WordFreq.get(dm.WordFreq.article == self.anarchy,
+                             dm.WordFreq.word == absurd)
+        assert wf.freq == 1
+
+
+
