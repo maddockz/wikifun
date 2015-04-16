f59afa59cd1540fcc75ea0e1be99b7366bb4c767
maddockz <maddockz@gmail.com>
2015-04-16 12:18:14 -0700

    Allow running of all etl scripts from command-line

---
+++ wikifun/etl/__main__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Running etl module from command-line will create database using information
+from config.py and begin uploading articles.  This module takes one
+command-line argument, which provides a bound on the number of articles to
+import.
+"""
+__author__ = 'maddockz'
+
+import sys
+
+import data_models as dm
+import parse
+import label
+import word_count as wc
+
+if __name__=='__main__':
+    if len(sys.argv) == 1:
+        limit = 1e9
+        print "No Limit"
+    else:
+        limit = sys.argv[1]
+        print "Limit = {0}".format(limit)
+
+    dm.create_database()
+    dm.create_tables()
+    parse.bz2_to_mysql(limit=limit)
+    wc.tabulate_articles(start=0, stop=limit)
+    label.label_history()
+
+
