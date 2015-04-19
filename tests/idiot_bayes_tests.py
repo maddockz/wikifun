269c70fa9bb6b9b5fb1ec11b312b8c93e0a7dd4d
maddockz <maddockz@gmail.com>
2015-04-19 01:05:05 -0700

    assert that word count is close (within 10) of sum of row in make_X function

--- tests/idiot_bayes_tests.py
+++ tests/idiot_bayes_tests.py
@@ -1,11 +1,20 @@
 __author__ = 'maddockz'
 
+import numpy as np
+
 import wikifun.model.idiots_bayes as ib
+import wikifun.etl.data_models as dm
+import wikifun.etl.word_count as wc
 
 class TestDataPreprocessing:
     def setup(self):
-        pass
+        self.article = ib.train[0]
 
-    def test_make_X(self):
-        X = ib.make_X(ib.train[0:3])
-        assert X.shape[0] == 3, X.shape[0]
+    def test_make_X_basic(self):
+        X = ib.make_X([self.article])
+        assert X.shape[0] == 1, X.shape[0]
+        word_count = np.sum(wc.count(self.article.text).values())
+        assert np.abs( np.sum(X.todense()) - word_count ) < 10
+
+    def test_make_y_basic(self):
+        assert ib.make_y([self.article])[0] == False
