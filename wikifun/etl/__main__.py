791eaf5211d94898798dffc894206a3bdd7053fd
maddockz <maddockz@gmail.com>
2015-04-19 16:37:18 -0700

    added drop_tables command so that no articles are uploaded twice

--- wikifun/etl/__main__.py
+++ wikifun/etl/__main__.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Running etl module from command-line will create database using information
-from config.py and begin uploading articles.  This module takes one
-command-line argument, which provides a bound on the number of articles to
-import.
+Warning: Running this module from command line will first drop all tables in
+database.
+
+Then will create database using information from config.py and begin
+uploading articles.  Will also generate simple random train and test samples,
+computing word frequencies for all articles in samples, before caching
+the results.
+
+This module takes one command-line argument, which provides a bound on the
+number of articles to import.
 """
 __author__ = 'maddockz'
 
@@ -24,6 +30,7 @@ if __name__=='__main__':
         print "Limit = {0}".format(limit)
 
     dm.create_database()
+    dm.drop_tables()
     dm.create_tables()
     parse.bz2_to_mysql(limit=limit)
     sample.main()
