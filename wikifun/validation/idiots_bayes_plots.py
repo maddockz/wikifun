b110d6752b02a6fbe908c7e071b13b2390be1230
maddockz <maddockz@gmail.com>
2015-04-19 14:55:42 -0700

    added limit argument

--- wikifun/validation/idiots_bayes_plots.py
+++ wikifun/validation/idiots_bayes_plots.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*_
 """
-Generate plots for validating naive bayes model in idiots_bayes.py
+Generate plots for validating the naive bayes model in idiots_bayes.py.
+Requires previously cached indices for training and test data
 """
 __author__ = 'maddockz'
 
@@ -10,13 +11,14 @@ import wikifun.model.idiots_bayes as ib
 import wikifun.etl.sample as sample
 
 
-def roc_curve():
-    nb_fit = ib.main()
+def roc_curve(limit = int(1e9)):
+    nb_fit = ib.main(limit)
     train_ids, test_ids = sample.get_cached_ids()
-    test = sample.get_articles(test_ids)
+    test = sample.get_articles(test_ids[:limit])
     X = ib.make_X(test)
     y = ib.make_y(test)
-    roc.plot_all([("test", y, nb_fit.predict_log_proba(X) )])
+    log_probas = nb_fit.predict_log_proba(X)[:,1]
+    roc.plot_all([("Naive Bayes", y, log_probas)])
 
 
 if __name__ == '__main__':
