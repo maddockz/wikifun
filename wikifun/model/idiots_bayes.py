fc2b807bba7b8989af8ea70769bc2558f9b7bb19
maddockz <maddockz@gmail.com>
2015-04-19 01:06:13 -0700

    added functions (including main() ) that fits sklearn.naive_bayes.Multinomial model to X,y data

--- wikifun/model/idiots_bayes.py
+++ wikifun/model/idiots_bayes.py
@@ -18,26 +18,27 @@ Parameters:
 Full Bayesian assumptions: (will implement in another module)
 * The likelihood F|L,p ~ Multi(L,p)
 * The entries of the parameter p are iid with Unif(0,1) prior distribution
-* An uninformative prior C ~ Bernoulli(1/2)
+* An uninformative prior C ~ Bernoulli(0.5)
 
 Simplified Idiot's assumptions: (implemented here)
 * L and C are independent (Likely an incredibly idiotic assumption!)
 * L and p are independent (Perhaps less unreasonable assumption)
-* Replace p with point estimates p_hat.  Ie instead of (ML) we use (smooth):
+* Replace p with point estimates p_hat, using (smooth) instead of (ML):
   * (ML) p_hat = N_C/N_tot
     where N's are word counts in category or total
   * (smooth) p_hat = (N_C + alpha)/ (N + alpha * n)
-    where alpha is param
+    where alpha is smoothing param
 
 Our classifier is then determined via a MAP (maximum a posteriori) estimate.
 One may use either the observed category frequency as point estimate for prior
 distributions of C:
   C ~ Bernoulli( # C-articles / # total articles ).
-Or one may choose not, and keep our uninformative prior C.
+Or one may choose not, and keep our uninformative prior C ~ Bernoulli(0.5).
 
-Note: The log of the (numerator) in the posterior density is linear in F,
+Note: The log of the (numerator of) the posterior density is linear in F,
 and therefore the decision boundary will be linear.
 """
+
 from sklearn.svm.libsvm import fit
 
 __author__ = 'maddockz'
@@ -45,42 +46,67 @@ __author__ = 'maddockz'
 import random
 from sklearn import naive_bayes as nb
 import numpy as np
+from scipy import sparse
 
-import peewee as pw
 
 import wikifun.etl.data_models as dm
+import wikifun.etl.sample as sample
 
-# Split all articles (with word data computed) into training and test data
-query = dm.WordFreq.select(dm.WordFreq.article).distinct()
-all_arts = [wf.article for wf in query]
-
-n = len(all_arts)
-n_train = (n * 3)/5
-
-train_idx = random.sample(xrange(n), n_train)
-train = [all_arts[i] for i in train_idx]
-test  = [all_arts[i] for i in xrange(n) if i not in train_idx]
-
-nbm = nb.MultinomialNB(alpha=1.0, fit_prior=True)
 
 def make_X(articles):
     """
-    Creates observation matrix X from a list of articles
+    Creates sparse training matrix X from a list of articles
     :param articles: list of [dm.Article]
-    :return: np.array
+    :return: sparse.coo.coo_matrix
     """
-    words = [word for word in dm.Word.select()]
-    print len(words)
-    n_words = len(words)
-    X = np.ndarray(shape=(0, n_words), dtype=int)
+    n_words = np.sum(1 for _ in dm.Word.select() )
+    X = sparse.lil_matrix((0, n_words), dtype=int)
     for article in articles:
-        print article.title
-        row = np.ndarray(shape=(1,n_words), dtype=int)
-        for i in xrange(n_words):
-#            q = dm.WordFreq.article == article & dm.WordFreq.word == words[i]
-            row[0,i] = 0 #dm.WordFreq.get(q).freq
-
-        X = np.concatenate((X,row), axis=0)
+        print u'Extracting article: {0}'.format(article.title)
+        row = np.ndarray((1,n_words), dtype=int)
+        row.fill(0)
+        # Iterate over all words in article
+        word_freqs = dm.WordFreq.select().where(dm.WordFreq.article == article)
+        for word_freq in word_freqs:
+            word_index = word_freq.word.id - 1
+            row[0,word_index] = word_freq.freq
+        if X.shape[0] == 0:
+            X = sparse.lil_matrix(row)
+        else:
+            X = sparse.vstack([X, sparse.lil_matrix(row)])
     return X
 
+def make_y(articles):
+    """
+    Creates training target matrix y from a list of articles.
+    :param articles:
+    :return: np.ndarray
+    """
+    n_arts = len(articles)
+    y = np.ndarray((n_arts,), dtype=bool)
+    y.fill(False)
+    for i, article in enumerate(articles):
+        if article.is_history:
+            y[i] = True
+    return y
 
+def fit_model(X, y):
+    nbm = nb.MultinomialNB(alpha=1.0, fit_prior=True)
+    print 'Fitting model on {0} articles...'.format(len(y))
+    nbm.fit(X,y)
+    print 'Model fit!'
+    return nbm
+
+def main():
+    """
+    Returns naive bayes model fit on cached (pickled) training data
+    :return: nb.MultinomialNB
+    """
+    train_ids, test_ids = sample.get_cached_ids()
+    train = sample.get_articles(train_ids)
+    X, y = make_X(train), make_y(train)
+    return fit_model(X, y)
+
+
+if __name__ == '__main__':
+    main()
